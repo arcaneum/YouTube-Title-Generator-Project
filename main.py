fd63c85e29b9ded5a999aecd1c9e1415a515533f
arcaneum <38038031+arcaneum@users.noreply.github.com>
2023-12-26 07:10:41 +0000

    Resolve merge conflict in .gitignore

---
+++ main.py
@@ -0,0 +1,56 @@
+import streamlit as st
+from pydantic import BaseModel
+from typing import List
+import openai
+from dotenv import load_dotenv
+import os
+
+# Load the .env file
+load_dotenv()
+
+# Set the OpenAI API key using the OpenAI client
+from openai import OpenAI
+openai_api_key = os.getenv("OPENAI_API_KEY")
+open_ai_client = OpenAI(api_key=openai_api_key)
+
+# Assuming the patch function is defined in the instructor module
+import instructor
+instructor.patch(open_ai_client)
+
+# Pydantic model for structured data validation
+class Titles(BaseModel):
+    titles: List[str]
+
+# Function to generate YouTube titles using structured_generator
+def structured_generator(openai_model, prompt, custom_model):
+    result: custom_model = open_ai_client.chat.completions.create(
+        model=openai_model, 
+        response_model=custom_model,
+        messages=[{"role": "user", "content": f"{prompt}, output must be in json"}]
+    )
+    return result
+
+# Streamlit app layout
+st.title("YouTube Title Generator")
+
+# Input field for topic
+topic = st.text_input("Enter Topic")
+
+# Button to generate titles
+if st.button("Generate Titles"):
+    if topic:
+        try:
+            # Call function to generate titles
+            prompt = f"Generate 5 creative YouTube title ideas for the topic: '{topic}'"
+            openai_model = "gpt-3.5-turbo"
+            result = structured_generator(openai_model, prompt, Titles)
+            # Displaying titles
+            if result.titles:
+                for title in result.titles:
+                    st.write(title)
+            else:
+                st.error("No titles were generated. Try a different topic.")
+        except Exception as e:
+            st.error(f"Error in generating titles: {e}")
+    else:
+        st.error("Please enter a topic.")
